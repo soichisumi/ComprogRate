2e420e9c172af6ae9e93d24c008c478104d4da48
yoyoyousei <yoyoyousei2016@gmail.com>
2017-07-17 02:06:16 +0900

    add rate-color mappting of topcoder. modify query parameter

--- getRates.js
+++ getRates.js
@@ -1,5 +1,5 @@
-let rp =require("request-promise")
-let chr = require("cheerio-httpcli") //jqueryに似た書き方でdom取得ができる
+let rp =require("request-promise") 
+let chr = require("cheerio-httpcli") //jqueryに似た書き方でdom操作ができる
 
 function getAtcoderRate(userId){
     //このpromiseでthenを呼ぶとcheerioのfetchが実行される。
@@ -29,11 +29,11 @@ function getCodeforcesRate(userId){
             json: true,
             uri: 'http://codeforces.com/api/user.info',
             qs:{
-                'handle': userId
+                'handles': userId
             }
         }).then(function(res){ 
             return new Promise(function(resolve, reject){
-                resolve({now: parseInt(res.rating), max: parseInt(res.maxRating)}) 
+                resolve({now: parseInt(res.result[0].rating), max: parseInt(res.result[0].maxRating)}) 
             })
         })
 }
@@ -45,7 +45,7 @@ function getTopcoderRate(userId){
             uri: 'http://api.topcoder.com/v2/users/' + userId + '/statistics/data/srm'
         }).then(function(res){ 
             return new Promise(function(resolve, reject){
-                resolve({now: parseInt(res.rating), max: parseInt(maximumRating)}) 
+                resolve({now: parseInt(res.rating), max: parseInt(res.maximumRating)}) 
             })
         })
 }
@@ -55,18 +55,25 @@ function getHtmlForRate(rate /*int*/, contest /*string*/){
     let classes
 
     switch(contest){
+        case 'codeforces':
+            thresholds = [1200, 1400, 1600, 1900, 2200, 2400, 10000]
+            classes = ['user-gray', 'user-green', 'user-cyan', 'user-blue',
+                    'user-violet', 'user-orange', 'user-red']
+            break
+        
+        case 'topcoder':
+            thresholds = [900, 1200, 1500, 2200, 10000]
+            classes = ['user-gray', 'user-green', 'user-blue', 'user-yerrow', 'user-red']
+            break
+
         case 'atcoder':
+        default:
                         //gray, brown, green, cyan, blue, yellow, orange, red 
             thresholds  = [400, 800, 1200, 1600, 2000, 2400, 2800, 10000]
             classes     = ['user-gray', 'user-brown', 'user-green', 'user-cyan',
                         'user-blue', 'user-yellow', 'user-orange', 'user-red' ]
             break
-        case 'codeforces':
-        default:
-            thresholds = [1200, 1400, 1600, 1900, 2200, 2400, 10000]
-            classes = ['user-gray', 'user-green', 'user-cyan', 'user-blue',
-                    'user-violet', 'user-orange', 'user-red']
-            break
+        
     }
     for (let i in thresholds){
         if(rate < thresholds[i])
@@ -74,11 +81,11 @@ function getHtmlForRate(rate /*int*/, contest /*string*/){
     }
 }
 
-//getAtcoderRate("yoyoyousei").then(function(res){console.log(res)
-//console.log(getHtmlForRate(res.now,"atcoder"))})
+getAtcoderRate("yoyoyousei").then(function(res){console.log(res)
+console.log(getHtmlForRate(res.now, "atcoder"))})
 
-// getCodeforcesRate('yousei').then(function(res){console.log(res)
-// console.log(getHtmlForRate(res.result[res.result.length-1].newRating))})
+getCodeforcesRate('yousei').then(function(res){console.log(res)
+console.log(getHtmlForRate(res.now, "codeforces"))})
 
-// getTopcoderRate('camshift').then(function(res){console.log(res)
-// console.log(getHtmlForRate(res.rating))})
+getTopcoderRate('camshift').then(function(res){console.log(res)
+console.log(getHtmlForRate(res.now, "topcoder"))})
