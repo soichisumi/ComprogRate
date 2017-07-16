cda404d68c129418edfa69b82336b8fa977c5623
yoyoyousei <yoyoyousei2016@gmail.com>
2017-07-16 22:49:58 +0900

    delete unused comments

--- getRates.js
+++ getRates.js
@@ -49,38 +49,5 @@ function getHtmlForRate(rate /*int*/, contest /*string*/){
     }
 }
 
-
-
-
-    // console.log("option: " + JSON.stringify(optionA))
-    // rp(optionA).then(function(error, response, body){
-    //         if(!error && response.statusCode == 200){
-                
-    //             let $ = chr.load(body);
-    //             let nowRate = parseInt( $('dd').eq(5).text() )
-    //             let maxRate = parseInt( $('dd').eq(6).text() )
-    //             console.log(Json.stringify({nowR:nowRate, maxR: maxRate}))
-    //             return {nowR:nowRate, maxR: maxRate}
-    //         }else {
-    //             console.log("--------------------------------------------------");
-    //             if (error && "code" in error) {
-    //                 console.log("Error Code:" + error.code);
-    //             }
-    //             if (error && "errno" in error) {
-    //                 console.log("Error No:" + error.errno);
-    //             }
-    //             if (error && "syscall" in error) {
-    //                 console.log("Error Syscall:" + error.syscall);
-    //             }
-    //             if (response && "statusCode" in response) {
-    //                 console.log("Status Code:" +  response.statusCode);
-    //             }
-    //         }
-    //     })
-    //     .catch(function(err){
-    //         console.log("error occured in promise");
-    //     })
-//}
-
 getAtcoderRate("yoyoyousei").then(function(res){console.log(res)
 console.log(getHtmlForRate(res.now,"atcoder"))})
