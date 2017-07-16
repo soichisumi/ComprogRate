fbf94a82f13f4e732798df18bc89587453ca931c
yoyoyousei <yoyoyousei2016@gmail.com>
2017-07-17 05:14:17 +0900

    fix

--- node/getRates.js
+++ node/getRates.js
@@ -83,11 +83,11 @@ function getHtmlForRate(rate /*int*/, contest /*string*/){
     }
 }
 
-// getAtcoderRate("yoyoyousei").then(function(res){console.log(res)
-// console.log(getHtmlForRate(res.now, "atcoder"))})
+getAtcoderRate("yoyoyousei").then(function(res){console.log(res)
+document.getElementById('atc').innerHtml(getHtmlForRate(res.now, "atcoder"))})
 
-// getCodeforcesRate('yousei').then(function(res){console.log(res)
-// console.log(getHtmlForRate(res.now, "codeforces"))})
+getCodeforcesRate('yousei').then(function(res){console.log(res)
+document.getElementById('cdf').innerHtml(getHtmlForRate(res.now, "codeforces"))})
 
-// getTopcoderRate('camshift').then(function(res){console.log(res)
-// console.log(getHtmlForRate(res.now, "topcoder"))})
+getTopcoderRate('camshift').then(function(res){console.log(res)
+document.getElementById('tpc').innerHtml(getHtmlForRate(res.now, "topcoder"))})
